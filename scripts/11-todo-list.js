2a8bda83ce86d325a2c26636ba93e8a6f69bca75
My_Name <joeldelcky@gmail.com>
2024-11-08 22:58:56 +0000

    suppression de 11-arrays-and-loop.html

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -3,14 +3,14 @@ const todolist = [];
 function AddTodo() {
   const inputElement = document.querySelector(".js-name-input");
   //met la valeur saisie dans le input element dans la variable 'name'
+
   const name = inputElement.value;
-  console.log(name);
-       
+  
   //ajoute les valeurs saisie et recuper dans la variable 'name' et mis dans le tableau "todolist"
 
   todolist.push(name);
 
-  //Dés que la valeur est saisie l'espace de saisie devient vide  ou encore
+  //vide le input pour la prochaine saisie
 
   inputElement.value = "";
 }
