06d2e8b5fd812500b4767ca561091fa867a2f55b
My_Name <joeldelcky@gmail.com>
2024-11-09 10:39:52 +0000

    update

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -2,15 +2,7 @@ const todolist = [];
 
 function AddTodo() {
   const inputElement = document.querySelector(".js-name-input");
-  //met la valeur saisie dans le input element dans la variable 'name'
-
-  const name = inputElement.value;
-  
-  //ajoute les valeurs saisie et recuper dans la variable 'name' et mis dans le tableau "todolist"
-
-  todolist.push(name);
-
-  //vide le input pour la prochaine saisie
+  console.log(inputElement.value);
 
   inputElement.value = "";
 }
